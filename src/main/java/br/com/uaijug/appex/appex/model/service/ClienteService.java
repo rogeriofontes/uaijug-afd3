85cc764ba1b5076638562a8d907100a7b22e4db9
Rogério Fontes <fontestz@gmail.com>
2018-07-28 16:34:07 -0300

    Adiçao de Service

---
+++ src/main/java/br/com/uaijug/appex/appex/model/service/ClienteService.java
@@ -0,0 +1,30 @@
+package br.com.uaijug.appex.appex.model.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.uaijug.appex.appex.dao.ClienteDAO;
+import br.com.uaijug.appex.appex.exception.CPFValidationException;
+import br.com.uaijug.appex.appex.model.domain.Cliente;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteDAO dao;
+
+	public void salvar(Cliente cliente) {
+		if (cliente.getCpf().length() < 11) {
+			throw new CPFValidationException("Erro na validação do cpf!");
+		} else {
+			dao.salvar(cliente);
+		}
+	}
+
+	public List<Cliente> listar() {
+		return dao.listar();
+	}
+
+}
